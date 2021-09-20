39657e760695676973ab98daa102cb1ec7e45668
RPbeee <sirakami.65536@gmail.com>
2021-09-20 16:18:36 +0900

    マニュアルを読み込むクラスを作った

--- src/main/java/net/github/rpbeee/gman/ManualControl.java
+++ src/main/java/net/github/rpbeee/gman/ManualControl.java
@@ -1,13 +1,41 @@
 package net.github.rpbeee.gman;
 
+import jdk.tools.jlink.plugin.Plugin;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class ManualControl {
 
-    private final File file;
-    private final String fileName;
+    private Path file;
+    private String fileName;
+    private String title;
+    private Plugin plugin;
 
-    public void ManualControl () {
+    public void ManualControl (Plugin plugin, String fileName) {
+        this.plugin = plugin;
+        this.fileName = fileName;
         return;
     }
+
+    public List<String> getString() {
+        this.file = Paths.get(fileName);
+        List<String> manual = null;
+        try {
+            manual = Files.readAllLines(this.file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return manual;
+    }
+
+    public boolean manualExists() {
+        File manual = new File(this.fileName);
+        return manual.exists();
+    }
+
 }
